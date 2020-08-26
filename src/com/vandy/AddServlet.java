e4b7e0b18c497c8dbf99a1c6d1d89b72b1a5882c
vandyG <vandy.goel23@gmail.com>
2020-08-26 20:45:48 +0530

    update AddServlet and index.html for handling post req
    add doPost method in AddServlet and post method in index.html instead of the default post

--- src/com/vandy/AddServlet.java
+++ src/com/vandy/AddServlet.java
@@ -13,7 +13,7 @@ public class AddServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
@@ -21,7 +21,8 @@ public class AddServlet extends HttpServlet {
 		int k = i + j;
 		
 //		System.out.println("result is " + j);
-		res.getWriter().println("result is " + j);
+		res.getWriter().println("result is " + k);
 	}
 
+	
 }
