12693c08be405d78aa4c12978f57cbace130a3c4
vandyG <vandy.goel23@gmail.com>
2020-08-26 23:34:27 +0530

    add sqrServlet and update addServlet
    add request dispatcher to AddServlet to call SqrServlet from it. set another attribute in req to add the sum of the two numbers to be passed to sqrServlet

--- src/com/vandy/AddServlet.java
+++ src/com/vandy/AddServlet.java
@@ -2,6 +2,8 @@ package com.vandy;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,16 +15,19 @@ public class AddServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
 		
-//		System.out.println("result is " + j);
-		res.getWriter().println("result is " + k);
+		req.setAttribute("k", k);
+		
+		//Two ways to call another servlet Req Dispatcher or redirect
+		//We'll use dispatcher
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sqr");
+		rd.forward(req, res);
 	}
-
-	
 }
---
+++ src/com/vandy/SqrServlet.java
@@ -0,0 +1,25 @@
+package com.vandy;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqrServlet extends HttpServlet{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter out = res.getWriter();
+		
+		int k = (int)req.getAttribute("k");
+		k *= k;
+		
+		out.println("Result is " + k);
+	}
+}
