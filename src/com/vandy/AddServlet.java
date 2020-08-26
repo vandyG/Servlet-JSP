3a2ee9be34ddb800f4fef40b4aa8585a7c145b66
vandyG <vandy.goel23@gmail.com>
2020-08-26 19:27:53 +0530

    update AddServlet.java
    add service method

--- src/com/vandy/AddServlet.java
+++ src/com/vandy/AddServlet.java
@@ -1,5 +1,23 @@
 package com.vandy;
 
-public class AddServlet {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public void service(HttpServletRequest req, HttpServletResponse res) {
+		
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		
+	}
 
 }
