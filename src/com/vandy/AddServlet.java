7baedb7a0cfe9241f1c8b013463c17c064817662
vandyG <vandy.goel23@gmail.com>
2020-08-26 19:40:23 +0530

    add code to print add response

--- src/com/vandy/AddServlet.java
+++ src/com/vandy/AddServlet.java
@@ -1,5 +1,7 @@
 package com.vandy;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,15 +13,15 @@ public class AddServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public void service(HttpServletRequest req, HttpServletResponse res) {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
 		
-		System.out.println("result is " + j);
-		
+//		System.out.println("result is " + j);
+		res.getWriter().println("result is " + j);
 	}
 
 }
